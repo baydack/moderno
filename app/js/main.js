0bff2ded0daa72435b16236900833dca4170deee
baydack <vasyabaydack0@gmail.com>
2020-07-24 15:40:52 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
 	$(".rate-star").rateYo({
 		rating: 5,
@@ -10,7 +10,7 @@ $(function(){
 		dots: true,
 		arrows: false,
 		slidesToShow: 4,
-		slidesToScroll: 4,		
+		slidesToScroll: 4,
 	});
 
 	$(".js-range-slider").ionRangeSlider({
@@ -22,17 +22,27 @@ $(function(){
 		prefix: "$"
 	});
 
-	$('.icon-th-list').on('click', function(){
+	$('.icon-th-list').on('click', function () {
 		$('.product__item').addClass('list');
 		$('.icon-th-list').addClass('active');
 		$('.icon-th-large').removeClass('active')
 	});
-	 
-	$('.icon-th-large').on('click', function (){
+
+	$('.icon-th-large').on('click', function () {
 		$('.product__item').removeClass('list');
 		$('.icon-th-large').addClass('active');
 		$('.icon-th-list').removeClass('active');
 	});
+
+	//.menu__btn
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').slideToggle();
+	});
+
+	$('.header__btn-menu').on('click', function () {
+		$('.header__box').toggleClass('active');
+	});
+
 
 
 	var mixer = mixitup('.product__inner-box');
